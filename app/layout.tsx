c7d2a9ffe9d804e288a3821538f01cd9e6111055
rafapecino <rafapecino@gmial.com>
2026-02-04 13:11:10 +0100

    feat(design): favicon añadido

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css"
 export const metadata: Metadata = {
   title: "PecinoGP - Análisis de MotoGP",
   description: "Tu comunidad de análisis técnicos, directos y cobertura exclusiva del campeonato mundial de MotoGP.",
+  icons: {
+    icon: "/favicon.png",
+  },
 }
 
 export default function RootLayout({
